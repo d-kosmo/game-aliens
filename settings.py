3516fb15ac54af4cf6a1971399c5e387ab6c146c
dmitry <dmitry@geocube.ru>
2017-03-09 23:47:50 +0500

    1

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 class Settings():
 
 	def __init__(self):
-		self.screen_width = 800
-		self.screen_height = 600
+		self.screen_width = 1024
+		self.screen_height = 768
 		self.bg_color = (230,230,230)
 		self.ship_speed_factor = 0.5
 		self.bullet_speed_factor = 0.5
