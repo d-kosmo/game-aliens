277a2e2ab78b6f29590790af4d32177ef3796b99
dmitry <dmitry@geocube.ru>
2017-03-14 00:19:25 +0500

    move
    moving

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 class Settings():
 
 	def __init__(self):
-		self.screen_width = 1024
-		self.screen_height = 768
+		self.screen_width = 800
+		self.screen_height = 600
 		self.bg_color = (230,230,230)
 		self.ship_speed_factor = 0.5
 		self.bullet_speed_factor = 0.5
@@ -11,3 +11,5 @@ class Settings():
 		self.bullet_color = (60,60,60)
 		self.bullets_allowed = 3
 		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		self.fleet_direction = 1
