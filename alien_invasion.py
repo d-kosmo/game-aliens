64d3b78d71d06735bc0c9a5879b68de0ffe1f267
dmitry <dmitry@geocube.ru>
2017-03-19 23:51:52 +0500

    play block

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,7 +21,7 @@ def run_game():
 	aliens = Group()
 	gf.create_fleet(ai_settings,screen,aliens,ship)
 	while True:
-                gf.check_events(ai_settings,screen,ship,bullets)
+                gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
                 if stats.game_active:
                         ship.update()
                         gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
